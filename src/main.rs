d38a0938f10eb510316d3b776182ce12c7f8065f
Jeffery Utter <jeff@jeffutter.com>
2024-05-18 23:23:56 -0500

    Better exit handling, split out a bit of code

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,27 @@ fn read_value(path: &str) -> Result<u32> {
     Ok(res)
 }
 
+fn update(
+    ambient_brightness: &mut AmbientBrightness,
+    kbd_brightness: &mut KBDBrightness,
+    screen_brightness: &mut ScreenBrightness,
+) -> Result<()> {
+    let new_val = ambient_brightness.update()?;
+    trace!("New Val POST: {}", new_val);
+    kbd_brightness.adjust(new_val)?;
+    screen_brightness.adjust(new_val)?;
+    Ok(())
+}
+
 fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init();
+    let (close_sender, close_receiver) = bounded(1);
+    ctrlc::set_handler(move || {
+        close_sender
+            .send(())
+            .expect("Could not send signal on channel.")
+    })
+    .expect("Error setting Ctrl-C handler");
     let args = Args::parse();
 
     if args.server {
@@ -55,62 +74,53 @@ fn main() -> Result<()> {
             .build()?;
 
         let mut ambient_brightness = AmbientBrightness::new()?.init()?;
-        let kbd_brightness = KBDBrightness::new(&proxy, "leds", "asus::kbd_backlight");
-        let screen_brightness = ScreenBrightness::new(&proxy, "backlight", "intel_backlight")?;
+        let mut kbd_brightness = KBDBrightness::new(&proxy, "leds", "asus::kbd_backlight");
+        let mut screen_brightness = ScreenBrightness::new(&proxy, "backlight", "intel_backlight")?;
         let (command_sender, command_receiver) = bounded(1);
 
-        thread::spawn(move || {
-            let res = || -> Result<()> {
-                let socket_path = Path::new(&env::temp_dir()).join("ambient_brightness.sock");
-                fs::remove_file(socket_path.clone())?;
-                let listener = UnixListener::bind(socket_path)?;
+        let _join_handle: thread::JoinHandle<Result<()>> = thread::spawn(move || {
+            let socket_path = Path::new(&env::temp_dir()).join("ambient_brightness.sock");
+            fs::remove_file(socket_path.clone())?;
+            let listener = UnixListener::bind(socket_path)?;
 
-                loop {
-                    let (mut socket, _addr) = listener.accept()?;
-                    let socket_read = socket.read_u8(Endian::native())?;
-                    debug!("Got Message: {}", socket_read);
+            loop {
+                let (mut socket, _addr) = listener.accept()?;
+                let socket_read = socket.read_u8(Endian::native())?;
+                debug!("Got Message: {}", socket_read);
 
-                    match socket_read {
-                        0 => command_sender.send(0)?,
-                        1 => command_sender.send(1)?,
-                        _ => (),
-                    }
+                match socket_read {
+                    0 => command_sender.send(0)?,
+                    1 => command_sender.send(1)?,
+                    _ => (),
                 }
-            }();
-
-            res.unwrap();
+            }
         });
 
         let ticker = tick(Duration::from_secs(5));
         let first = after(Duration::from_millis(1));
 
-        let update = |ambient_brightness: &mut AmbientBrightness| -> Result<()> {
-            let new_val = ambient_brightness.update()?;
-            trace!("New Val POST: {}", new_val);
-            kbd_brightness.adjust(new_val)?;
-            screen_brightness.adjust(new_val)?;
-            Ok(())
-        };
-
         loop {
             select! {
+                recv(close_receiver) -> _ => {
+                    break
+                },
                 recv(command_receiver) -> msg => match msg? {
                     0 => {
                         ambient_brightness.dim();
-                        update(&mut ambient_brightness)?
+                        update(&mut ambient_brightness, &mut kbd_brightness, &mut screen_brightness)?
                     },
 
                     1 => {
                         ambient_brightness.undim();
-                        update(&mut ambient_brightness)?
+                        update(&mut ambient_brightness, &mut kbd_brightness, &mut screen_brightness)?
                     },
                     _ => ()
                 },
                 recv(ticker) -> _  => {
-                    update(&mut ambient_brightness)?
+                        update(&mut ambient_brightness, &mut kbd_brightness, &mut screen_brightness)?
                 },
                 recv(first) -> _ => {
-                    update(&mut ambient_brightness)?
+                        update(&mut ambient_brightness, &mut kbd_brightness, &mut screen_brightness)?
                 },
             }
         }
